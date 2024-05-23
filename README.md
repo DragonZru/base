02ffb4326afba948bc8cd87618be3e21e0fea2e6
ylli <ylli.shanks@gmail.com>
2024-05-23 19:01:09 +0800

    feature: 新增redisConfiguration

--- README.md
+++ README.md
@@ -6,10 +6,13 @@
 ## db 管理工具 flyway 
 [spring data migration properties](https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html#appendix.application-properties.data-migration)
 
-
 ## Gson
 [Gson User Guide](https://github.com/google/gson/blob/main/UserGuide.md)
 
 ## maven 私服
 [nexus](https://hub.docker.com/r/sonatype/nexus3?uuid=F523A7E2-1684-416A-AED6-EEF3021A7F49)
 初始密码: cat nexus-data/admin.password
+
+## mybatis mapper 
+新版mapper5，可以参考mapper4 https://github.com/abel533/Mapper/wiki
+[mybatis mapper](https://mapper.mybatis.io/docs/v2.x/1.getting-started.html#_1-1-%E4%B8%BB%E8%A6%81%E7%9B%AE%E6%A0%87)
--- src/main/java/com/ylli/api/config/redis/RedisConfiguration.java
+++ src/main/java/com/ylli/api/config/redis/RedisConfiguration.java
@@ -1,7 +1,58 @@
 package com.ylli.api.config.redis;
 
+import io.lettuce.core.ReadFrom;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfiguration {
+
+    @Bean
+    @ConditionalOnBean(RedisClusterConfiguration.class)
+    public StringRedisTemplate stringRedisTemplate(RedisClusterConfiguration redisClusterConfiguration) {
+        // 优先主读，fail back to replica
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .readFrom(ReadFrom.MASTER_PREFERRED)
+                .build();
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisClusterConfiguration, clientConfig);
+        lettuceConnectionFactory.start();
+        return new StringRedisTemplate(lettuceConnectionFactory);
+    }
+
+    @Bean
+    @ConditionalOnBean(ReactiveRedisConnectionFactory.class)
+    public ReactiveStringRedisTemplate reactiveStringRedisTemplate(ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
+        return new ReactiveStringRedisTemplate(reactiveRedisConnectionFactory);
+    }
+
+    @Bean
+    @ConditionalOnBean(RedisClusterConfiguration.class)
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory(RedisClusterConfiguration redisClusterConfiguration) {
+        // 优先主读，fail back to replica
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .readFrom(ReadFrom.MASTER_PREFERRED)
+                .build();
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisClusterConfiguration, clientConfig);
+        lettuceConnectionFactory.start();
+        return lettuceConnectionFactory;
+    }
+
+    @Bean
+    public RedisClusterConfiguration redisClusterConfiguration(ObjectProvider<RedisProperties> redisPropertiesProvider) {
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(redisPropertiesProvider.getIfAvailable().getCluster().getNodes());
+        //redisClusterConfiguration.setUsername("root");
+        //redisClusterConfiguration.setPassword("123456");
+        return redisClusterConfiguration;
+    }
 }
