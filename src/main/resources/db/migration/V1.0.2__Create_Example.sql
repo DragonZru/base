199a2bcd0a028333aa9f098cad86330691232bf9
ylli <ylli.shanks@gmail.com>
2024-06-03 21:40:17 +0800

    refactor: 添加 mysql JSON_OVERLAPS() 索引支持

--- src/main/resources/db/migration/V1.0.2__Create_Example.sql
+++ src/main/resources/db/migration/V1.0.2__Create_Example.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `example`.`t_example`
+CREATE TABLE IF NOT EXISTS `t_example`
 (
     `id`          bigint UNSIGNED NOT NULL AUTO_INCREMENT,
     `username`    varchar(255)    NOT NULL,
@@ -11,3 +11,4 @@ CREATE TABLE IF NOT EXISTS `example`.`t_example`
     PRIMARY KEY (`id`),
     UNIQUE INDEX `U_username`(`username`)
 );
+ALTER TABLE t_example ADD INDEX n_extras( ( CAST( extras -> '$[*].serialNo' AS UNSIGNED ARRAY)) );
