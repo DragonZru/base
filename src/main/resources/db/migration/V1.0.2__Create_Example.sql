ddaa22a60e621eba7b038b19f497f9da5e15c548
ylli <ylli.shanks@gmail.com>
2024-06-07 15:56:44 +0800

    refactor: mysql json 索引生效必要条件

--- src/main/resources/db/migration/V1.0.2__Create_Example.sql
+++ src/main/resources/db/migration/V1.0.2__Create_Example.sql
@@ -12,5 +12,12 @@ CREATE TABLE IF NOT EXISTS `t_example`
     UNIQUE INDEX `U_username`(`username`)
 );
 -- ALTER TABLE t_example ADD INDEX n_extras( ( CAST( extras -> '$[*].serialNo' AS UNSIGNED ARRAY)) );
--- 支持string,utf8mb4_bin 区分大小写,
+
+/*
+ * 支持string,utf8mb4_bin 区分大小写,若想索引生效需要指定COLLATE utf8mb4_bin
+ * The ->> operator is the same as JSON_UNQUOTE(JSON_EXTRACT(...))
+ * CAST() returns a string with the collation utf8mb4_0900_ai_ci (the server default collation).
+ * JSON_UNQUOTE() returns a string with the collation utf8mb4_bin (hard coded).
+ * https://dev.mysql.com/doc/refman/8.0/en/create-index.html
+ */
 ALTER TABLE t_example ADD INDEX n_extras( ( CAST( extras ->> '$[*].serialNo' AS CHAR(30)) COLLATE utf8mb4_bin) );
