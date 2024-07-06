e521ad911391ddcae55dfdc8a81e13716e5676e4
ylli <ylli.shanks@gmail.com>
2024-07-06 13:49:22 +0800

    refactor: 移除druid

--- leaf-starter/src/main/java/com/ylli/api/LeafApplication.java
+++ leaf-starter/src/main/java/com/ylli/api/LeafApplication.java
@@ -1,13 +1,13 @@
-package com.ylli.any;
+package com.ylli.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnyStarterApplication {
+public class LeafApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AnyStarterApplication.class, args);
+        SpringApplication.run(LeafApplication.class, args);
     }
 
 }
--- src/main/java/com/ylli/api/BaseApplication.java
+++ src/main/java/com/ylli/api/BaseApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.TimeZone;
 
 @SpringBootApplication
-public class BasisApplication {
+public class BaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasisApplication.class, args);
+		SpringApplication.run(BaseApplication.class, args);
 	}
 
 	@PostConstruct
