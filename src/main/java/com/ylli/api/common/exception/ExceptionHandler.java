a0fa76e11e8111a095c822b0d919cbb50fd80fe1
ylli <ylli.shanks@gmail.com>
2024-07-26 16:11:30 +0800

    feature: add logstash

--- src/main/java/com/ylli/api/common/exception/ExceptionHandler.java
+++ src/main/java/com/ylli/api/common/exception/ExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.ylli.api.common.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,30 +12,30 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-@Slf4j
 @RestControllerAdvice
 public class ExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
 
     @Value("${debugMsg.enable:false}")
     boolean debug;
 
     @org.springframework.web.bind.annotation.ExceptionHandler(GenericException.class)
     public ResponseEntity<?> exceptionHandler(GenericException ex) {
-        log.error("GenericException exceptionHandler", ex);
-        if (debug) {
-            return ResponseEntity.status(ex.getCode()).body(new ResponseBody(ex.getCode(), ex.getMessage(), printStackTrace(ex)));
-        }
-        return ResponseEntity.status(ex.getCode()).body(new ResponseBody(ex.getCode(), ex.getMessage()));
+        String printStackTrace = printStackTrace(ex);
+        logger.error(printStackTrace);
+        return ResponseEntity.status(ex.getCode()).body(new ResponseBody(ex.getCode(), ex.getMessage(), debug ? printStackTrace : null));
     }
 
 
     @org.springframework.web.bind.annotation.ExceptionHandler
     public ResponseEntity<?> exceptionHandler(Exception ex) {
-        log.error("Exception exceptionHandler", ex);
+        String printStackTrace = printStackTrace(ex);
+        logger.error(printStackTrace);
         //默认503.
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ResponseBody(HttpStatus.SERVICE_UNAVAILABLE.value(), ex.getMessage(), printStackTrace(ex)));
+                .body(new ResponseBody(HttpStatus.SERVICE_UNAVAILABLE, ex.getMessage(), printStackTrace(ex)));
     }
 
     private String printStackTrace(Exception ex) {
@@ -52,13 +53,14 @@ public class ExceptionHandler {
         public String message;
         public String debugMsg;
 
-        public ResponseBody(int code, String message) {
-            this.code = code;
-            this.message = message;
-        }
-
         public ResponseBody(int code, String message, String debugMsg) {
             this.code = code;
+            this.message = message;
+            this.debugMsg = debugMsg;
+        }
+
+        public ResponseBody(HttpStatus httpStatus, String message, String debugMsg) {
+            this.code = httpStatus.value();
             this.message = message;
             this.debugMsg = debugMsg;
         }
