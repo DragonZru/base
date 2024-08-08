4b274b6223ee1092c4a62af2c0331f4cfe651248
ylli <ylli.shanks@gmail.com>
2024-08-08 23:51:09 +0800

    refactor: webclient configuration 配置示例，自定义最大pending count

--- src/main/java/com/ylli/api/config/WebClientConfiguration.java
+++ src/main/java/com/ylli/api/config/WebClientConfiguration.java
@@ -6,7 +6,9 @@ import org.springframework.cloud.client.loadbalancer.reactive.DeferringLoadBalan
 import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 /**
  * https://docs.spring.io/spring-cloud-commons/docs/current/reference/html/#webflux-with-reactive-loadbalancer
@@ -24,12 +26,19 @@ public class WebClientConfiguration {
                 new DeferringLoadBalancerExchangeFilterFunction<>(reactorLoadBalancerExchangeFilterFunctionProvider);
 
         return WebClient.builder()
-//                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create(
+                        //https://stackoverflow.com/questions/68640474/facing-issue-webclientrequestexception-pending-acquire-queue-has-reached-its-m
+//                                ConnectionProvider.builder("connectionPool")
+//                                        .maxConnections(1000)
+//                                        .pendingAcquireMaxCount(Integer.MAX_VALUE)
+//                                        .build()
+                )
 //                        .proxy(proxy -> proxy.type(ProxyProvider.Proxy.HTTP))
 //                        .resolver(spec -> {
 //                            //查询超时时间，默认5s.
 //                            spec.queryTimeout(Duration.ofMillis(2000));
-//                        })))
+//                        })
+                ))
                 .filter(filterFunction);
     }
 
