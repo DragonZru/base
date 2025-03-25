5adf95856fa7889d0bd7660188b551f51ff1dbd0
ylli <ylli.shanks@gmail.com>
2025-03-25 11:16:45 +0800

    test: eventListener

--- src/main/java/com/ylli/api/test/EventListener.java
+++ src/main/java/com/ylli/api/test/EventListener.java
@@ -2,6 +2,7 @@ package com.ylli.api.test;
 
 import com.google.gson.Gson;
 import com.ylli.api.config.model.ConfigModel;
+import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
@@ -11,8 +12,18 @@ import org.springframework.stereotype.Component;
 public class EventListener {
 
     @Async
+//    @org.springframework.context.event.EventListener(condition = "#event.name.equals('ylli')")
     @org.springframework.context.event.EventListener
+    @Order(1)
     public void onEvent(ConfigModel event) {
-        System.out.println(Thread.currentThread().getName() + ", event:" + new Gson().toJson(event));
+//        throw new RuntimeException("test");
+        System.out.println(Thread.currentThread().getName() + ", event1:" + new Gson().toJson(event));
+    }
+
+    @Async
+    @org.springframework.context.event.EventListener
+    @Order(2)
+    public void onEvent1(ConfigModel event) {
+        System.out.println(Thread.currentThread().getName() + ", event2:" + new Gson().toJson(event));
     }
 }
