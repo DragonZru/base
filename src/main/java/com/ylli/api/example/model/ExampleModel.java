c5c0493a3546a6400765dc17be8d2c6729cc67d8
ylli <ylli.shanks@gmail.com>
2025-04-15 17:12:38 +0800

    fix: mysql 单机版可以正常启动，get查询

--- src/main/java/com/ylli/api/example/model/ExampleModel.java
+++ src/main/java/com/ylli/api/example/model/ExampleModel.java
@@ -1,22 +1,22 @@
 package com.ylli.api.example.model;
 
-//import io.mybatis.provider.Entity;
+import io.mybatis.provider.Entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 import java.util.List;
 
-//@Entity.Table("t_example")
+@Entity.Table("t_example")
 @Data
 @NoArgsConstructor
 // Use a different name for Example.class, because it defined in mapper
 //broadcast table
 public class ExampleModel {
 
-//    @Entity.Column(id = true, updatable = false
-////            , genId = SnowFlakeGenerator.class
-//    )
+    @Entity.Column(id = true, updatable = false
+//            , genId = SnowFlakeGenerator.class
+    )
     //@Entity.Column(id = true, updatable = false) = @Id 使用mysql自增 or 自定义id 生成策略
     public Long id;
 
