f3fae0a9a59abdc02cfb030e30cae88701bb8ba2
ylli <ylli.shanks@gmail.com>
2025-06-16 11:03:00 +0800

    feat: rocketmq init
    TODO 深度集成

--- src/main/java/com/ylli/base/configuration/rocketmq/RocketMQ.java
+++ src/main/java/com/ylli/base/configuration/rocketmq/RocketMQ.java
@@ -1,74 +1,74 @@
-package com.ylli.base.configuration.rocketmq;
-
-import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.client.producer.LocalTransactionState;
-import org.apache.rocketmq.client.producer.TransactionListener;
-import org.apache.rocketmq.client.producer.TransactionMQProducer;
-import org.apache.rocketmq.client.producer.TransactionSendResult;
-import org.apache.rocketmq.common.message.Message;
-import org.apache.rocketmq.common.message.MessageExt;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.stereotype.Component;
-
-import java.util.Optional;
-import java.util.function.Function;
-
-/**
- * @author ylli
- */
-@Component
-public class RocketMQ {
-
-    ObjectProvider<TransactionMQProducer> transactionProducerProvider;
-
-    public RocketMQ(ObjectProvider<TransactionMQProducer> transactionProducerProvider) {
-        this.transactionProducerProvider = transactionProducerProvider;
-    }
-
-    public RocketMQ() {
-    }
-
-    public TransactionSendResult sendTransactionMessage(Message message, Function<String, Boolean> transaction, Function<String, Boolean> check) throws MQClientException {
-        return sendTransactionMessage(getTransactionMQProducer(null), message, transaction, check);
-    }
-
-    public TransactionSendResult sendTransactionMessage(String group, Message message, Function<String, Boolean> transaction, Function<String, Boolean> check) throws MQClientException {
-        return sendTransactionMessage(getTransactionMQProducer(group), message, transaction, check);
-    }
-
-    public TransactionMQProducer getTransactionMQProducer(String group) {
-        return transactionProducerProvider.stream().filter(producer ->
-        {
-            return (Optional.ofNullable(group).orElse("defaultTransactionProducerGroup")).equals(producer.getProducerGroup());
-        }).findFirst().orElseThrow(() -> new RuntimeException(group + " not found"));
-    }
-
-    public TransactionSendResult sendTransactionMessage(TransactionMQProducer transactionMQProducer,
-                                                        Message message,
-                                                        Function<String, Boolean> saveTransaction,
-                                                        Function<String, Boolean> localCheck) throws MQClientException {
-
-        transactionMQProducer.setTransactionListener(new TransactionListener() {
-            @Override
-            public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
-                //执行本地事物
-                if (saveTransaction != null && saveTransaction.apply(new String(msg.getBody()))) {
-                    return LocalTransactionState.COMMIT_MESSAGE;
-                }
-                return LocalTransactionState.ROLLBACK_MESSAGE;
-            }
-
-            @Override
-            public LocalTransactionState checkLocalTransaction(MessageExt msg) {
-                // 根据事务执行状态，返回对应的事务状态
-                // 返回值可以是COMMIT_MESSAGE、ROLLBACK_MESSAGE或UNKNOW
-                if (localCheck != null && localCheck.apply(new String(msg.getBody()))) {
-                    // 本地事物查询
-                    return LocalTransactionState.COMMIT_MESSAGE;
-                }
-                return LocalTransactionState.UNKNOW;
-            }
-        });
-        return transactionMQProducer.sendMessageInTransaction(message, null);
-    }
-}
+//package com.ylli.base.configuration.rocketmq;
+//
+//import org.apache.rocketmq.client.exception.MQClientException;
+//import org.apache.rocketmq.client.producer.LocalTransactionState;
+//import org.apache.rocketmq.client.producer.TransactionListener;
+//import org.apache.rocketmq.client.producer.TransactionMQProducer;
+//import org.apache.rocketmq.client.producer.TransactionSendResult;
+//import org.apache.rocketmq.common.message.Message;
+//import org.apache.rocketmq.common.message.MessageExt;
+//import org.springframework.beans.factory.ObjectProvider;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Optional;
+//import java.util.function.Function;
+//
+///**
+// * @author ylli
+// */
+//@Component
+//public class RocketMQ {
+//
+//    ObjectProvider<TransactionMQProducer> transactionProducerProvider;
+//
+//    public RocketMQ(ObjectProvider<TransactionMQProducer> transactionProducerProvider) {
+//        this.transactionProducerProvider = transactionProducerProvider;
+//    }
+//
+//    public RocketMQ() {
+//    }
+//
+//    public TransactionSendResult sendTransactionMessage(Message message, Function<String, Boolean> transaction, Function<String, Boolean> check) throws MQClientException {
+//        return sendTransactionMessage(getTransactionMQProducer(null), message, transaction, check);
+//    }
+//
+//    public TransactionSendResult sendTransactionMessage(String group, Message message, Function<String, Boolean> transaction, Function<String, Boolean> check) throws MQClientException {
+//        return sendTransactionMessage(getTransactionMQProducer(group), message, transaction, check);
+//    }
+//
+//    public TransactionMQProducer getTransactionMQProducer(String group) {
+//        return transactionProducerProvider.stream().filter(producer ->
+//        {
+//            return (Optional.ofNullable(group).orElse("defaultTransactionProducerGroup")).equals(producer.getProducerGroup());
+//        }).findFirst().orElseThrow(() -> new RuntimeException(group + " not found"));
+//    }
+//
+//    public TransactionSendResult sendTransactionMessage(TransactionMQProducer transactionMQProducer,
+//                                                        Message message,
+//                                                        Function<String, Boolean> saveTransaction,
+//                                                        Function<String, Boolean> localCheck) throws MQClientException {
+//
+//        transactionMQProducer.setTransactionListener(new TransactionListener() {
+//            @Override
+//            public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
+//                //执行本地事物
+//                if (saveTransaction != null && saveTransaction.apply(new String(msg.getBody()))) {
+//                    return LocalTransactionState.COMMIT_MESSAGE;
+//                }
+//                return LocalTransactionState.ROLLBACK_MESSAGE;
+//            }
+//
+//            @Override
+//            public LocalTransactionState checkLocalTransaction(MessageExt msg) {
+//                // 根据事务执行状态，返回对应的事务状态
+//                // 返回值可以是COMMIT_MESSAGE、ROLLBACK_MESSAGE或UNKNOW
+//                if (localCheck != null && localCheck.apply(new String(msg.getBody()))) {
+//                    // 本地事物查询
+//                    return LocalTransactionState.COMMIT_MESSAGE;
+//                }
+//                return LocalTransactionState.UNKNOW;
+//            }
+//        });
+//        return transactionMQProducer.sendMessageInTransaction(message, null);
+//    }
+//}
